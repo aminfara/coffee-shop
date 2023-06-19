d8c0c75a24d7c5a498ab580a95b4a5c8a808c603
Ali 🚶 <ali.aminfar@gmail.com>
2023-06-19 17:51:14 +1000

    Add a simple helloworld lambda

--- packages/infra/src/stack.ts
+++ packages/infra/src/stack.ts
@@ -8,6 +8,12 @@ export class CdkStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+    new cdk.aws_lambda.Function(this, 'coffee-service', {
+      runtime: cdk.aws_lambda.Runtime.NODEJS_18_X,
+      code: cdk.aws_lambda.Code.fromAsset('../backend/dist'),
+      handler: 'handler.handler',
+    });
+
     // example resource
     // const queue = new sqs.Queue(this, 'CdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
